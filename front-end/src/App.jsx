ee7379b97ebdba2db84f96f99f0313f883d1e2a0
Michael Greason <mgreason18@gmail.com>
2023-08-11 10:52:26 -0400

    setup routes for header

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,11 +1,18 @@
 import { useState } from "react";
 import Landing from "./pages/landing";
 import "./style/App.css";
+import { Routes, Route } from "react-router-dom";
+import Header from "./components/header";
 
 function App() {
   return (
     <>
-      <Landing />
+      <Header />
+      <div>
+        <Routes>
+          <Route path="/home" element={<Landing />} />
+        </Routes>
+      </div>
     </>
   );
 }
