6f9819cc9e40efe5e4da1080c6a6a21b5f796629
Michael Greason <mgreason18@gmail.com>
2023-08-14 11:18:48 -0400

    header applied

--- front-end/src/components/header.jsx
+++ front-end/src/components/header.jsx
@@ -2,17 +2,28 @@ import { Menu } from "@mui/base/Menu";
 import { MenuButton } from "@mui/base/MenuButton";
 import { MenuItem } from "@mui/base/MenuItem";
 import { Dropdown } from "@mui/base/Dropdown";
+import { useNavigate } from "react-router-dom";
 
 export default function Header() {
+  const navigate = useNavigate();
+
+  function goHome() {
+    navigate("/");
+  }
+
   return (
     <>
-      <div className="mt-4 mx-4 mb-10">
-        <div className=" flex justify-end">
+      <div className="mt-4 mx-4 mb-10 flex  justify-between">
+        <div className=" cursor-pointer" onClick={goHome}>
+          Hit and Run Studios
+        </div>
+        <div className="">
           <Dropdown>
-            <MenuButton>Settings</MenuButton>
+            <MenuButton>Explore</MenuButton>
             <Menu>
-              <MenuItem className="mr-4">My account</MenuItem>
-              <MenuItem className="mr-4">Notification preferences</MenuItem>
+              <MenuItem className="mr-4 my-2">Listen</MenuItem>
+              <MenuItem className="mr-4 my-2">Photos</MenuItem>
+              <MenuItem className="mr-4 my-2">Reviews</MenuItem>
             </Menu>
           </Dropdown>
         </div>
--- front-end/src/pages/landing.jsx
+++ front-end/src/pages/landing.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Landing() {
   return (
     <>
-      <div>Welcome to the studio</div>
+      <div className=" text-center text-3xl">Welcome to the studio</div>
     </>
   );
 }
