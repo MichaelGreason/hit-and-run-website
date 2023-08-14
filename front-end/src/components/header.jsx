d059fb11eea721789cbc096c69a9f6903286ba0d
Michael Greason <mgreason18@gmail.com>
2023-08-14 11:06:09 -0400

    building header in material base  ui

--- front-end/src/components/header.jsx
+++ front-end/src/components/header.jsx
@@ -1,47 +1,22 @@
-import React from "react";
-import { MegaMenu } from "primereact/megamenu";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faMusic } from "@fortawesome/free-solid-svg-icons";
-import { Menu } from "primereact/menu";
-import { useRef } from "react";
-import { faHeadphones } from "@fortawesome/free-solid-svg-icons";
-import { Button } from "primereact/button";
+import { Menu } from "@mui/base/Menu";
+import { MenuButton } from "@mui/base/MenuButton";
+import { MenuItem } from "@mui/base/MenuItem";
+import { Dropdown } from "@mui/base/Dropdown";
 
 export default function Header() {
-  const menuRight = useRef(null);
-  const start = <FontAwesomeIcon icon={faMusic} />;
-  const items = {
-    label: "Listen",
-    icon: <FontAwesomeIcon icon={faHeadphones} />,
-  };
-  const end = (
-    <>
-      <Menu
-        model={items}
-        popup
-        ref={menuRight}
-        id="popup_menu_right"
-        popupAlignment="right"
-      />
-      <Button
-        label="Show Right"
-        icon="pi pi-align-right"
-        className="mr-2"
-        onClick={(event) => menuRight.current.toggle(event)}
-        aria-controls="popup_menu_right"
-        aria-haspopup
-      />
-    </>
-  );
-
   return (
-    <div className="card">
-      <MegaMenu
-        orientation="horizontal"
-        start={start}
-        end={end}
-        breakpoint="960px"
-      />
-    </div>
+    <>
+      <div className="mt-4 mx-4 mb-10">
+        <div className=" flex justify-end">
+          <Dropdown>
+            <MenuButton>Settings</MenuButton>
+            <Menu>
+              <MenuItem className="mr-4">My account</MenuItem>
+              <MenuItem className="mr-4">Notification preferences</MenuItem>
+            </Menu>
+          </Dropdown>
+        </div>
+      </div>
+    </>
   );
 }
