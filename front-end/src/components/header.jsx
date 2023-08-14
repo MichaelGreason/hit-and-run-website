7bd95660e06c20624aab2ca7bbba46a96269370b
Michael Greason <mgreason18@gmail.com>
2023-08-14 10:37:58 -0400

    adding header

--- front-end/src/components/header.jsx
+++ front-end/src/components/header.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { MegaMenu } from "primereact/megamenu";
 import { InputText } from "primereact/inputtext";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMusic } from "@fortawesome/free-solid-svg-icons";
 
 export default function Header() {
-  const start = <FontAwesomeIcon icon="fa-solid fa-music" />;
+  const start = <FontAwesomeIcon icon={faMusic} />;
   const end = <InputText placeholder="Search" type="text" />;
 
   return (
