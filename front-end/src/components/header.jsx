ad162cc3c658ddbb97928dbe2a32cafc528784a0
Michael Greason <mgreason18@gmail.com>
2023-08-14 10:29:02 -0400

    installed font awesome

--- front-end/src/components/header.jsx
+++ front-end/src/components/header.jsx
@@ -1,1 +1,16 @@
-export default function Header() {}
+import React from "react";
+import { MegaMenu } from "primereact/megamenu";
+import { InputText } from "primereact/inputtext";
+
+export default function Header() {
+  return (
+    <div className="card">
+      <MegaMenu
+        orientation="horizontal"
+        start={start}
+        end={end}
+        breakpoint="960px"
+      />
+    </div>
+  );
+}
