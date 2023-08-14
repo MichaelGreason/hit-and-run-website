faf3bf4ed9a7a0e34d58bb012066f18cd43472da
Michael Greason <mgreason18@gmail.com>
2023-08-14 10:59:37 -0400

    installed material base

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,18 +1,20 @@
 import { useState } from "react";
 import Landing from "./pages/landing";
 import "./style/App.css";
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/header";
 
 function App() {
   return (
     <>
-      <Header />
-      <div>
-        <Routes>
-          <Route path="/home" element={<Landing />} />
-        </Routes>
-      </div>
+      <Router>
+        <Header />
+        <div>
+          <Routes>
+            <Route path="/" element={<Landing />} />
+          </Routes>
+        </div>
+      </Router>
     </>
   );
 }
--- front-end/src/components/header.jsx
+++ front-end/src/components/header.jsx
@@ -1,12 +1,38 @@
 import React from "react";
 import { MegaMenu } from "primereact/megamenu";
-import { InputText } from "primereact/inputtext";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMusic } from "@fortawesome/free-solid-svg-icons";
+import { Menu } from "primereact/menu";
+import { useRef } from "react";
+import { faHeadphones } from "@fortawesome/free-solid-svg-icons";
+import { Button } from "primereact/button";
 
 export default function Header() {
+  const menuRight = useRef(null);
   const start = <FontAwesomeIcon icon={faMusic} />;
-  const end = <InputText placeholder="Search" type="text" />;
+  const items = {
+    label: "Listen",
+    icon: <FontAwesomeIcon icon={faHeadphones} />,
+  };
+  const end = (
+    <>
+      <Menu
+        model={items}
+        popup
+        ref={menuRight}
+        id="popup_menu_right"
+        popupAlignment="right"
+      />
+      <Button
+        label="Show Right"
+        icon="pi pi-align-right"
+        className="mr-2"
+        onClick={(event) => menuRight.current.toggle(event)}
+        aria-controls="popup_menu_right"
+        aria-haspopup
+      />
+    </>
+  );
 
   return (
     <div className="card">
