25a4a245ad51cd9492aefa992b83ebbe3efc075a
Michael Greason <mgreason18@gmail.com>
2023-08-15 17:59:04 -0400

    added photos

--- front-end/src/pages/photos.jsx
+++ front-end/src/pages/photos.jsx
@@ -1,7 +1,9 @@
 export default function Photos() {
   return (
     <div className="site-background">
-      <div className=" text-center">Photos!</div>
+      <div className="cards">
+        <img src="/src/assets/studio1.JPG" />
+      </div>
     </div>
   );
 }
