5fc3ed6074968e9e3626cd7c61ed5769409319bb
Michael Greason <mgreason18@gmail.com>
2023-08-15 16:35:02 -0400

    fitting pages

--- front-end/src/pages/landing.jsx
+++ front-end/src/pages/landing.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Landing() {
   return (
     <>
-      <div className="landing h-screen">
+      <div className="landing">
         <div className="text-center text-3xl">Welcome to the studio</div>
       </div>
     </>
