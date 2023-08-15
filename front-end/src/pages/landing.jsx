a1862ede94eeb5ef20aec4fde85c3b7c951c9bd1
Michael Greason <mgreason18@gmail.com>
2023-08-15 17:12:25 -0400

    landing setup

--- front-end/src/pages/landing.jsx
+++ front-end/src/pages/landing.jsx
@@ -4,13 +4,16 @@ export default function Landing() {
   return (
     <>
       <div className="site-background">
-        <div className="text-center text-4xl font-extrabold">
+        <div className="text-center text-6xl font-extrabold">
           Welcome to the studio
         </div>
         <div className="cards">
-          Hi, my name is Michael Greason. I am the owner and Hit and Run Studios
-          here in Orange County, NC. I focus on delivering an amazing product at
-          a low cost. This is what I love to do.
+          Hi, my name is Michael Greason. I am the owner and operator of Hit and
+          Run Studios here in Orange County, NC. I focus on delivering an
+          amazing product at a low cost. This is what I love to do.
+        </div>
+        <div className="cards">
+          <img src="/src/assets/dsc04626.JPG" alt="MG photo" />
         </div>
       </div>
     </>
