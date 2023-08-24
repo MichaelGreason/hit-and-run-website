9b003d146879d99e67b856af8b4c047da263e757
Michael Greason <mgreason18@gmail.com>
2023-08-24 15:23:39 -0400

    adio page

--- front-end/src/pages/listen.jsx
+++ front-end/src/pages/listen.jsx
@@ -2,6 +2,16 @@ export default function Listen() {
   return (
     <div className="site-background">
       <div className=" text-center titles">Listen!</div>
+      <div className="">
+        <div>Forgetmyname</div>
+        <audio controls>
+          <source
+            src="https://dl.dropboxusercontent.com/home/Nikki%20Lux/Five+F_V_1.mp3
+            "
+            type="audio/mpeg"
+          />
+        </audio>
+      </div>
     </div>
   );
 }
