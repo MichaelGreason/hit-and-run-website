39b4e06c98317c063cc43f526c77da6bfd4d73f7
Michael Greason <mgreason18@gmail.com>
2023-08-25 10:42:17 -0400

    sound cloud embedd

--- front-end/src/pages/listen.jsx
+++ front-end/src/pages/listen.jsx
@@ -1,18 +1,21 @@
 import React from "react";
 
+function LoversLane() {
+  return (
+    <div className="flex justify-center">
+      <iframe
+        width="75%"
+        height="150"
+        src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1601006970%3Fsecret_token%3Ds-4COiMTBSq9W&color=%23080414&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"
+      ></iframe>
+    </div>
+  );
+}
 export default function Listen() {
   return (
     <div className="site-background">
       <div className=" text-center titles">Listen!</div>
-      <div className="">
-        <div>Forgetmyname</div>
-        <audio controls>
-          <source
-            src="https://dl.dropboxusercontent.com/home/Nikki%20Lux/Five+F_V_1.mp3"
-            type="audio/mpeg"
-          />
-        </audio>
-      </div>
+      <LoversLane />
     </div>
   );
 }
