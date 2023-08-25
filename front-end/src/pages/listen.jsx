70d8f4a279a95e16b0b1cde9e70e6d75aa841639
Michael Greason <mgreason18@gmail.com>
2023-08-25 11:05:20 -0400

    adding music

--- front-end/src/pages/listen.jsx
+++ front-end/src/pages/listen.jsx
@@ -1,13 +1,38 @@
 import React from "react";
 
+function Forgetmyname() {
+  return (
+    <div className="flex justify-center music">
+      <div className=" bg-black">Forgetmyname - Pop</div>
+    </div>
+  );
+}
 function LoversLane() {
   return (
-    <div className="flex justify-center">
-      <iframe
-        width="75%"
-        height="150"
-        src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1601006970%3Fsecret_token%3Ds-4COiMTBSq9W&color=%23080414&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"
-      ></iframe>
+    <div className="flex justify-center music">
+      <div className=" bg-black">
+        Lovers Lane - Acoustic
+        <iframe
+          width="100%"
+          height="150"
+          src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1601006970%3Fsecret_token%3Ds-4COiMTBSq9W&color=%23080414&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"
+        ></iframe>
+      </div>
+    </div>
+  );
+}
+
+function Beats() {
+  return (
+    <div className="flex justify-center music">
+      <div className=" bg-black">
+        Beats/ Instrumentals
+        <iframe
+          width="100%"
+          height="150"
+          src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1601022984%3Fsecret_token%3Ds-i1dkEitYeny&color=%23080414&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"
+        ></iframe>
+      </div>
     </div>
   );
 }
@@ -16,6 +41,7 @@ export default function Listen() {
     <div className="site-background">
       <div className=" text-center titles mb-20">Listen!</div>
       <LoversLane />
+      <Beats />
     </div>
   );
 }
