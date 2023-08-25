4bb67e7ca1df491be43e422202f131145f3f47bc
Michael Greason <mgreason18@gmail.com>
2023-08-25 11:19:09 -0400

    added more music

--- front-end/src/pages/listen.jsx
+++ front-end/src/pages/listen.jsx
@@ -1,9 +1,31 @@
 import React from "react";
 
+function TheWild() {
+  return (
+    <div className="flex justify-center music">
+      <div className=" bg-black">
+        The Wild - Hardcore/ Metalcore
+        <iframe
+          width="100%"
+          height="150"
+          src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1601036496%3Fsecret_token%3Ds-ClHe1ACIRI3&color=%23080414&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"
+        ></iframe>
+      </div>
+    </div>
+  );
+}
+
 function Forgetmyname() {
   return (
     <div className="flex justify-center music">
-      <div className=" bg-black">Forgetmyname - Pop</div>
+      <div className=" bg-black">
+        Forgetmyname - Pop
+        <iframe
+          width="100%"
+          height="150"
+          src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1601028654%3Fsecret_token%3Ds-77RQ91esyLM&color=%23080414&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"
+        ></iframe>
+      </div>
     </div>
   );
 }
@@ -40,6 +62,8 @@ export default function Listen() {
   return (
     <div className="site-background">
       <div className=" text-center titles mb-20">Listen!</div>
+      <Forgetmyname />
+      <TheWild />
       <LoversLane />
       <Beats />
     </div>
